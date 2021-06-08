cd1bbb6952a3b79e626653813cb150057575e3dd
gary <gary.white@lodgit.com>
2021-06-08 17:16:08 +0200

    code

--- View.js
+++ View.js
@@ -1,7 +1,13 @@
+function View(canvas) {
+	this.canvas = canvas;
+}
+
 View.prototype.updateDisplay = function() {
 	var view = this;
 	var context = view.canvas.getContext("2d");
 	context.clearRect(0, 0, view.canvas.width, view.canvas.height);
 	context.fillStyle = "black";
-	context.fillRect(0, 0, view.canvas.width, view.canvas.height);
+	context.fillRect(0, 0, view.canvas.width, view.canvas.height); // set background to cover canvas area
+    // context.strokeStyle = "orange";
+    // context.strokeRect(canvas.width-110, canvas.height-110, 100, 100)
 };
