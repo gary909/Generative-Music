dd943d4fd400c8312ba85370fbafe8481f40968d
gary <gary.white@lodgit.com>
2021-06-21 17:05:13 +0200

    import audio

--- musicbox.js
+++ musicbox.js
@@ -1,8 +1,31 @@
 window.onload = function() {
-    var canvas = document.getElementById("canvas");
-    var view = new View(canvas);
 
-    canvas.addEventListener("click", view.handleClick.bind(view), false);
+    var bufferLoader = new BufferLoader(
+        Audio.audioContext,
+        [
+            "A4.mp3",
+            "A5.mp3",
+            "C4.mp3",
+            "C5.mp3",
+            "D4.mp3",
+            "D5.mp3",
+            "E4.mp3",
+            "E5.mp3",
+            "G4.mp3",
+            "G5.mp3",
+        ],
+        finishedLoading
+    );
+    bufferLoader.load();
 
-    setInterval(view.updateDisplay.bind(view), view.frameRate);;
-}
+    function finishedLoading(bufferList) {
+        Audio.init(bufferList);
+
+        var canvas = document.getElementById("canvas");
+        var view = new View(canvas);
+
+        canvas.addEventListener("mousedown", view.handleClick.bind(view), false);
+        setInterval(view.updateDisplay.bind(view), view.frameRate);
+    }
+
+};
